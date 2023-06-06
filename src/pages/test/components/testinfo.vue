d6da8c07f9ff5a37a00d3a29a9a7f241710f3281
qt <123456a>
2023-06-06 11:26:54 +0800

    测试组件onMounted事件

---
+++ src/pages/test/components/testinfo.vue
@@ -0,0 +1,17 @@
+<template>
+  <view>testinfo</view>
+</template>
+
+<script setup>
+import {onMounted} from 'vue'
+onMounted(()=>{
+  uni.showToast({
+    title: 'mount2',
+    icon:'error',
+    duration: 4000
+  });
+})
+</script>
+
+<style scoped>
+</style>
--- src/pages/test/index.vue
+++ src/pages/test/index.vue
@@ -1,8 +1,18 @@
 <template>
   <view>index</view>
+  <testinfo></testinfo>
 </template>
 
 <script setup>
+import {onMounted} from 'vue'
+import testinfo from './components/testinfo.vue'
+onMounted(()=>{
+  // uni.showToast({
+  //   title: 'mount1',
+  //   icon:'error',
+  //   duration: 2000
+  // });
+})
 </script>
 
 <style lang="scss" scoped>
