c4fa2d6622ca744d36cab018e8acc3efe2f49eaf
qt <123456a>
2023-06-07 10:58:56 +0800

    小程序登录逻辑调整

---
+++ src/pages/index/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <view>
+    <view>
+      token:{{loginToken.accessToken}}
+    </view>
+    <view>
+      <navigator url="/pages/login/phone" open-type="redirect">去登录</navigator>
+      <navigator url="/pages/login/logout" open-type="redirect">退出登录</navigator>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import {ref,onMounted} from 'vue'
+const loginToken = ref({})
+onMounted(()=>{
+  loginToken.value = getApp().globalData.loginToken
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
