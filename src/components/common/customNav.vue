327b1e77058f594388803ad9fa62a2ad548bbb6c
qt <123456a>
2023-06-08 14:00:33 +0800

    作品详情页面

--- src/components/common/customNav.vue
+++ src/components/common/customNav.vue
@@ -1,6 +1,6 @@
 <template>
   <view>
-    <view class="fixed z-999999 w-screen bg-white top-0 left-0">
+    <view class="fixed z-[999999] w-screen bg-white top-0 left-0">
       <view class="bg-white w-screen" :style="{height:pageData.statusBarHeight+'px'}"></view>
       <view class="flex items-center bg-white w-screen" :style="{height:pageData.titleBarHeight+'px'}">
         <slot></slot>
--- src/pages/opus/index.vue
+++ src/pages/opus/index.vue
@@ -2,9 +2,9 @@
     <view>
       <customNav>
         <view @click="gotoBack"><uni-icons type="back" size="24"></uni-icons></view>
-        <view class="flex items-center">
+        <view class="flex items-center" @click="gotoMine">
           <image class="w-24 h-24 rounded-full flex-none" :src="pageData.detail.icon"/>
-          <view class="name mx-10 text-14 line-clamp-1">{{pageData.detail.author}}</view>
+          <view class="name mx-6 text-14 line-clamp-1">{{pageData.detail.author}}</view>
         </view>
         <button class="flex-none mr-100 w-64 h-26 leading-26 rounded-full bg-[#4ba1f8] active:bg-[#3194f9] text-white text-12">+关注</button>
       </customNav>
@@ -35,11 +35,15 @@ const pageData = reactive({
   }
 })
 
-const gotoBack = () => {
+const gotoBack = ()=>{
   uni.navigateBack({delta: 1})
 }
 
-const getData = () => {
+const gotoMine = ()=>{
+  uni.switchTab({url:'/pages/mine/mine?id='+pageData.detail.createdBy})
+}
+
+const getData = ()=>{
   getDataApi({id:pageData.id})
 }
 
