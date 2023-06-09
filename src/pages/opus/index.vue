b31fd7d991e01644e88d73c8ce664cb75d9a291c
qt <123456a>
2023-06-09 11:54:21 +0800

    作品页面加分享

--- src/pages/opus/index.vue
+++ src/pages/opus/index.vue
@@ -10,6 +10,27 @@
       </customNav>
       <opus-article :detail="pageData.detail" v-if="pageData.detail.opusType == 1"></opus-article>
       <!--opus-video :detail="pageData.detail" v-else-if="pageData.detail.opusType == 2"></opus-video-->
+      <view class="fixed bottom-0 h-40 w-screen bg-white">
+        <view class="mx-20 flex justify-between mt-7">
+          <view class="flex" @click="showApp">
+            <view class="flex">
+              <img class="w-24 h-24" src="@/static/opus/icon_star.png">
+              <view class="ml-5 mt-2">{{pageData.detail.collectionNum}}</view>
+            </view>
+            <view class="flex ml-20">
+              <img class="w-24 h-24" src="@/static/opus/icon_comment.png">
+              <view class="ml-5 mt-2">{{pageData.detail.commentNum}}</view>
+            </view>
+            <view class="flex ml-20">
+              <img class="w-24 h-24" src="@/static/opus/icon_heart.png">
+              <view class="ml-5 mt-2">{{pageData.detail.likeNum}}</view>
+            </view>
+          </view>
+          <view>
+            <button open-type="share"><img class="w-24 h-24" src="@/static/opus/icon_return.png"></button>
+          </view>
+        </view>
+      </view>
 
       <loginPop :isShow="pageData.isShowLoginPop" @close="pageData.isShowLoginPop = false"></loginPop>
     </view>
@@ -44,7 +65,13 @@ const onFollow = ()=>{
 }
 
 const gotoBack = ()=>{
-  uni.navigateBack({delta: 1})
+  let pages = getCurrentPages()
+  if (pages.length == 1) {
+    uni.switchTab({url:'/pages/index/index'})
+  }
+  else {
+    uni.navigateBack({delta: 1})
+  }
 }
 
 const gotoMine = ()=>{
@@ -61,4 +88,15 @@ const getDataApi = (params)=>{
     })
 }
 
+const onShareAppMessage = ()=>{
+  return {
+    title:pageData.detail.brief,
+    path:'/pages/opus/index?id='+pageData.id,
+    imageUrl:pageData.detail.cover.itemType == 2 ? pageData.detail.cover.content : pageData.detail.cover.thumbnail
+  }
+}
+
+const onShareTimeline = ()=>{
+  return onShareAppMessage()
+}
 </script>
