165e777f12b0d638b9b81175d532345620c7763e
qt <123456a>
2023-06-14 17:12:06 +0800

    瀑布流多个状态优化

---
+++ src/components/index/waterfallItemTitle.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="w-172" v-if="item && item.cover">
+    <image :src="item.cover.itemType == 2 ? item.cover.content : item.cover.thumbnail" :style="{width:'100%',height:computedHeight(item.cover.width,item.cover.height)+'rpx'}"></image>
+    <view class="">
+      <view class="">{{item.title}}</view>
+    </view>
+  </view>
+  <view class="h-14"></view>
+</template>
+
+<script setup>
+import {computed} from 'vue'
+const props = defineProps(['item'])
+const computedHeight = computed({
+  get: (w, h) => {
+    return function(w, h) {
+      return w <= 0 ? 344 : h*(344/w)
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
