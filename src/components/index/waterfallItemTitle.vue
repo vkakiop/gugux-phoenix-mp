bc5a8397f704b623fba093ce257ade8a80fcf774
lww <1532329365@qq.com>
2023-06-15 09:53:31 +0800

    跳转页面

--- src/components/index/waterfallItemTitle.vue
+++ src/components/index/waterfallItemTitle.vue
@@ -1,7 +1,7 @@
 <template>
   <view class="w-172" v-if="item && item.cover">
     <view v-if="isVirtualCal" :style="{width:'100%',height:computedHeight(item.cover.width,item.cover.height)+'rpx'}"></view>
-    <image v-else :src="item.cover.itemType == 2 ? item.cover.content : item.cover.thumbnail" :style="{width:'100%',height:computedHeight(item.cover.width,item.cover.height)+'rpx'}"></image>
+    <image v-else :src="item.cover.itemType == 2 ? item.cover.content : item.cover.thumbnail" :style="{width:'100%',height:computedHeight(item.cover.width,item.cover.height)+'rpx'}" @click="godetail(item)"></image>
     <view class="">
       <view class="">{{item.title}}</view>
     </view>
@@ -19,6 +19,17 @@ const computedHeight = computed({
     }
   }
 })
+const godetail=(item)=>{
+ if (item.cover.itemType == 2) {
+ 	uni.navigateTo({
+ 		url: `/pages/opus/index?id=${item.id}`
+ 	})
+ } else if (item.cover.itemType == 3) {
+ 	uni.navigateTo({
+ 		url: '/pages/VideoCarousel/VideoCarousel?id=' + item.id
+ 	})
+ }
+}
 </script>
 
 <style lang="scss" scoped>
