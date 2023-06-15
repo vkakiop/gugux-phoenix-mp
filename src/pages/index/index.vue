24e822a5840c249dd772cda768beb3018f626d36
lww <1532329365@qq.com>
2023-06-15 16:44:07 +0800

    吸顶

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,14 @@
 <template>
   <view class="bg-gray-100">
-    <view class="relative p-10  mb-10 ">
-      <icon type="search" size="26" class="absolute right-0" @click="skipHistory" />
+    <view class="fixed -top-5 z-50 bg-white w-full py-10 " @click="gohistory">
+      <view>
+        <view class="flex items-center  bg-[#F7F7F7] w-300 rounded-40 border-1 border-[#E3E3E3]">
+          <icon type="search" size="26" class="ml-10" />
+          <input class="bg-[#F7F7F7] ml-10  w-660 " v-model="searchvalue" placeholder="搜索" type="text"  disabled/>
+        </view>
+      </view>
     </view>
-    <view>
+    <view class="fixed top-40 z-50 bg-white w-full">
       <view class="uni-common-mt">
         <view style="flex:2">
           <u-tabs :list="computedMenuItems" lineWidth="40" lineColor="#f56c6c" :activeStyle="{
@@ -13,12 +18,9 @@
           }" :inactiveStyle="{
   color: '#606266',
   transform: 'scale(1)'
-}" itemStyle="padding-left: 15rpx; padding-right: 15rpx; height: 66rpx;" @click="(item)=>changeWaterfall(item.index)">
+}" itemStyle="padding-left: 15rpx; padding-right: 15rpx; height: 66rpx;"
+            @click="(item) => changeWaterfall(item.index)">
           </u-tabs>
-        </view>
-        <view class="flex items-center">更多
-          <uni-icons type="bottom" size="26" @click="drowDown('top')" v-if="iconType == 'bottom'"></uni-icons>
-          <uni-icons type="top" size="26" @click="drowDown('bottom')" v-else></uni-icons>
         </view>
       </view>
     </view>
@@ -31,15 +33,16 @@
         </uni-grid-item>
       </uni-grid>
     </view>
-    <view>
-    <view>
-      <view v-for="(waterItem,waterIndex) in pageData.waterfallItems">
-        <view v-show="waterIndex == pageData.currentIndex">
-          <waterfall :isComplete="waterItem.isComplete" :itemType="waterItem.itemType" :value="waterItem.items" :waterIndex="waterIndex" :currentIndex="pageData.currentIndex">
-          </waterfall>
+    <view class="pt-80">
+      <view>
+        <view v-for="(waterItem, waterIndex) in pageData.waterfallItems">
+          <view v-show="waterIndex == pageData.currentIndex">
+            <waterfall :isComplete="waterItem.isComplete" :itemType="waterItem.itemType" :value="waterItem.items"
+              :waterIndex="waterIndex" :currentIndex="pageData.currentIndex">
+            </waterfall>
+          </view>
         </view>
       </view>
-    </view>
     </view>
     <u-modal :show="show" @confirm="confirmShow" @cancel="closeShow" title="紧急通知" confirmColor="#D9001B"
       cancelColor="#0000FF" showCancelButton ref="uModal" confirmText="去导航" cancelText="查看详情">
@@ -52,38 +55,14 @@
 </template>
 
 <script setup>
-import { ref, onMounted, reactive,watch,computed,getCurrentInstance } from 'vue'
+import { ref, onMounted, reactive, watch, computed, getCurrentInstance } from 'vue'
 import { opusList } from '@/api/opus/list'
 import waterfall from '@/components/index/waterfall.vue'
-import { onShow,onReachBottom,onPageScroll } from "@dcloudio/uni-app"
+import { onShow, onReachBottom, onPageScroll } from "@dcloudio/uni-app"
 const isShow = ref(true)
 const internalInstance = getCurrentInstance()
 const iconType = ref('bottom')
-const menuList = reactive([{
-  name: '推荐',
-}, {
-  name: '文章',
-}, {
-  name: '视频'
-},
-{
-  name: '游记'
-},
-{
-  name: '车友圈'
-},
-{
-  name: '逛吃'
-}
-])
-	const paramsForm = reactive({
-		"keyword": "",
-		"pageNum": 1,
-		"pageSize": 10,
-		"searchTime": "",
-		"type": 0
-	})
-		const waterlist = ref([])
+const waterlist = ref([])
 const show = ref(false);
 const content = ref('');
 content.value = '您的好友等第十三月(1511837394)在重庆市四川商会触发了紧急通知，请点击电话联系或导航前往。'
@@ -91,7 +70,7 @@ const confirmShow = () => {
   show.value = false;
   console.log('去导航');
   // uni.navigateTo({ url: '/pages/emergencycontact/index?id=' + encodeURIComponent(13333333333) })
-  uni.navigateTo({ url: '/pages/safeguard/gonavigation?id=' + encodeURIComponent('1622360605426032996') })
+  uni.navigateTo({ url: '/pages/safeguard/gonavigation?id=' + encodeURIComponent(123) })
 }
 const closeShow = () => {
   show.value = false;
@@ -99,47 +78,51 @@ const closeShow = () => {
   uni.navigateTo({ url: '/pages/safeguard/safeguarddetail?id=' + encodeURIComponent(123) })
 }
 
-	const skipHistory = () => {
-		uni.navigateTo({
-			url: '/pages/index/searchHistory'
-		})
-	}
-onMounted(()=>{
+const gohistory = () => {
+  uni.navigateTo({
+    url: '/pages/index/searchHistory'
+  })
+}
+onMounted(() => {
   changeWaterfall(0)
 })
 
 const pageData = reactive({
-  scrollTop:0,
-  currentIndex:0,
-  waterfallItems : [
-    {scrollTop:0,isComplete:false,isLoading:false,itemType:'title',name:'推荐',items:[],query:{
-        path:{category:'0',pageNum:1,pageSize:10},
-        data:{passTime:''}
+  scrollTop: 0,
+  currentIndex: 0,
+  waterfallItems: [
+    {
+      scrollTop: 0, isComplete: false, isLoading: false, itemType: 'title', name: '推荐', items: [], query: {
+        path: { category: '0', pageNum: 1, pageSize: 10 },
+        data: { passTime: '' }
       }
     },
-    {scrollTop:0,isComplete:false,isLoading:false,itemType:'image',name:'徒步',items:[],query:{
-        path:{category:'2431436580328327949',pageNum:1,pageSize:10},
-        data:{passTime:''}
+    {
+      scrollTop: 0, isComplete: false, isLoading: false, itemType: 'image', name: '徒步', items: [], query: {
+        path: { category: '2431436580328327949', pageNum: 1, pageSize: 10 },
+        data: { passTime: '' }
       }
     },
-    {scrollTop:0,isComplete:false,isLoading:false,name:'风景',items:[],query:{
-        path:{category:'1622581366744965137',pageNum:1,pageSize:10},
-        data:{passTime:''}
+    {
+      scrollTop: 0, isComplete: false, isLoading: false, name: '风景', items: [], query: {
+        path: { category: '1622581366744965137', pageNum: 1, pageSize: 10 },
+        data: { passTime: '' }
       }
     },
-    {scrollTop:0,isComplete:false,isLoading:false,name:'骑行',items:[],query:{
-        path:{category:'1622581366744965136',pageNum:1,pageSize:10},
-        data:{passTime:''}
+    {
+      scrollTop: 0, isComplete: false, isLoading: false, name: '骑行', items: [], query: {
+        path: { category: '1622581366744965136', pageNum: 1, pageSize: 10 },
+        data: { passTime: '' }
       }
     },
   ],
 })
 
-const computedMenuItems = computed(()=>{
-  return pageData.waterfallItems.map(item=>{return {name:item.name}})
+const computedMenuItems = computed(() => {
+  return pageData.waterfallItems.map(item => { return { name: item.name } })
 })
 
-const changeWaterfall = (waterIndex)=>{
+const changeWaterfall = (waterIndex) => {
   if (pageData.currentIndex != waterIndex) {
     //读取滚动条高度
     pageData.waterfallItems[pageData.currentIndex].scrollTop = pageData.scrollTop
@@ -161,13 +144,13 @@ const getData = () => {
   let currentIndex = pageData.currentIndex
   pageData.waterfallItems[currentIndex].isLoading = true
   let query = pageData.waterfallItems[currentIndex].query
-  opusList(query.path,query.data).then(res => {
+  opusList(query.path, query.data).then(res => {
     if (res.data.page == res.data.totalPage) {
       pageData.waterfallItems[currentIndex].isComplete = true
     }
     pageData.waterfallItems[currentIndex].items = pageData.waterfallItems[currentIndex].items.concat(res.data.list)
     pageData.waterfallItems[currentIndex].isLoading = false
-  }).catch(e=>{
+  }).catch(e => {
     pageData.waterfallItems[currentIndex].isLoading = false
   })
 }
@@ -175,19 +158,19 @@ const getData = () => {
 onReachBottom(() => {
   let currentIndex = pageData.currentIndex
   if (!pageData.waterfallItems[currentIndex].isComplete && !pageData.waterfallItems[currentIndex].isLoading) {
-    pageData.waterfallItems[currentIndex].query.path.pageNum ++
+    pageData.waterfallItems[currentIndex].query.path.pageNum++
     getData()
   }
 })
 
-onPageScroll((res)=>{
+onPageScroll((res) => {
   pageData.scrollTop = res.scrollTop
 })
 </script>
 
 <style lang="scss" scoped>
+
 .uni-common-mt {
-  margin-top: 30rpx;
   display: flex;
   align-items: center;
   justify-content: space-between;
