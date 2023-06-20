6bc13c190a47c4e619713bf43e744206ec5e5e13
lww <1532329365@qq.com>
2023-06-20 10:43:18 +0800

    修改首页背景色

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <view class="bg-gray-100">
+  <view >
     <view class="fixed -top-5 z-50 bg-white w-full py-10 mb-14" >
       <view class="py-15">
         <view class="flex items-center ml-14  bg-[#fff] ml-10 h-39 w-302 rounded-40 border-1 border-[#E3E3E3] text-14" @click="gohistory">
@@ -19,7 +19,7 @@
 	  </view>
     </view>
 
-    <view class="pt-140 bg-gray-100">
+    <view class="pt-140">
       <view>
         <view v-for="(waterItem, waterIndex) in pageData.waterfallItems" :key="waterIndex">
           <view v-show="waterIndex == pageData.currentIndex">
