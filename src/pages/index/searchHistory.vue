69de070d12fc488602726aaa3406ec932ca548ff
lww <1532329365@qq.com>
2023-06-20 11:43:11 +0800

    修改搜索历史页

--- src/pages/index/searchHistory.vue
+++ src/pages/index/searchHistory.vue
@@ -6,7 +6,8 @@
 				<view class="search ">
 					<view class="flex items-center  bg-[#F7F7F7] w-300 rounded-40 border-1 border-[#E3E3E3]">
 						<icon type="search" size="15" class="ml-10" />
-						<input class="bg-[#F7F7F7] ml-10  w-220" v-model="searchvalue" @confirm="search" placeholder="请输入搜索关键字" type="text" />
+						<input class="bg-[#F7F7F7] ml-10  w-220" v-model="searchvalue" @confirm="search"
+							placeholder="请输入搜索关键字" type="text" />
 					</view>
 					<view @click="search">搜索</view>
 				</view>
@@ -15,9 +16,11 @@
 			<!-- 搜索历史 -->
 			<view class="searchHistory" v-if="isShowHistory">
 				<view v-if="pageData.searchHistoryList.length">
-					<view style="display: flex;align-items: center;justify-content: space-between;box-sizing: border-box;padding: 0px 5px;">
+					<view
+						style="display: flex;align-items: center;justify-content: space-between;box-sizing: border-box;padding: 0px 5px;">
 						<view>搜索历史:</view>
-						<view style="color: red;font-size: 28px;" @click="empty"><uni-icons type="trash" size="30"></uni-icons>
+						<view style="color: red;font-size: 28px;" @click="empty"><uni-icons type="trash"
+								size="30"></uni-icons>
 						</view>
 					</view>
 					<view class="searchHistoryItem">
@@ -36,8 +39,9 @@
 			<!-- 菜单 -->
 			<view class="w-full bg-[#fff] py-10" v-show="!isShowHistory">
 				<view class="flex ml-10">
-					<view v-for="(waterItem, index) in pageData.waterfallItems" :key="index"   class="mr-28 " @click="changeWaterfall(index)">
-						<view :class="pageData.currentIndex == index?'active':'inactive'">{{waterItem.name}}</view>
+					<view v-for="(waterItem, index) in pageData.waterfallItems" :key="index" class="mr-28 "
+						@click="changeWaterfall(index)">
+						<view :class="pageData.currentIndex == index ? 'active' : 'inactive'">{{ waterItem.name }}</view>
 						<view class=" h-4 relative -top-5 ">
 							<image src="/static/mine/line.png" class="w-30 h-4 " v-show="pageData.currentIndex == index" />
 						</view>
@@ -48,13 +52,14 @@
 			<!-- 菜单 -->
 		</view>
 		<view class="pt-100" v-show="!isShowHistory">
-			<view v-for="(waterItem,waterIndex) in pageData.waterfallItems" :key="waterIndex">
-				<view v-if="!waterItem.items.length&&waterIndex == pageData.currentIndex">
+			<view v-for="(waterItem, waterIndex) in pageData.waterfallItems" :key="waterIndex">
+				<view v-if="!waterItem.items.length && waterIndex == pageData.currentIndex">
 					<u-empty mode="search" icon="http://cdn.uviewui.com/uview/empty/search.png">
 					</u-empty>
 				</view>
 				<view v-show="waterIndex == pageData.currentIndex">
-					<waterfall :isComplete="waterItem.isComplete" :itemType="waterItem.itemType" :value="waterItem.items" :waterIndex="waterIndex" :currentIndex="pageData.currentIndex">
+					<waterfall :isComplete="waterItem.isComplete" :itemType="waterItem.itemType" :value="waterItem.items"
+						:waterIndex="waterIndex" :currentIndex="pageData.currentIndex">
 					</waterfall>
 				</view>
 			</view>
@@ -63,226 +68,182 @@
 </template>
 
 <script setup>
-	import waterfall from '@/components/index/waterfall.vue'
-	import { opusSearchNew } from "@/api/worksSearch/index.js"
-	import { ref, onMounted, reactive, watch, computed, getCurrentInstance } from 'vue'
-	import { onReachBottom, onLoad, onPageScroll, onShow } from '@dcloudio/uni-app';
-	import useLoginTokenStore from '@/store/modules/loginToken'
-	const loginTokenStore = useLoginTokenStore()
-	const internalInstance = getCurrentInstance()
-	const searchvalue = ref('')
-	const isShowHistory = ref(true)
-	const waterlist = ref([])
-	onMounted(() => {
-				changeWaterfall(0)
-		uni.getStorage({
-			key: 'gugusearchList',
-			success: function(res) {
-				pageData.searchHistoryList = JSON.parse(res.data)
-			}
-		})
-	})
-	const pageData = reactive({
-		searchHistoryList: [],
-		scrollTop: 0,
-		currentIndex: 0,
-		waterfallItems: [{
-				scrollTop: -1,
-				isComplete: false,
-				isLoading: false,
-				itemType: 'title',
-				name: '综合',
-				items: [],
-				query: {
-					path: {
-						type: 1,
-						pageNum: 1,
-						pageSize: 10,
-						searchTime: "",
-						keyword: ''
-					},
-					data: {
-						passTime: ''
-					}
-				}
-			},
-			{
-				scrollTop: -1,
-				isComplete: false,
-				isLoading: false,
-				itemType: 'title',
-				name: '文章',
-				items: [],
-				query: {
-					path: {
-						type: 2,
-						pageNum: 1,
-						pageSize: 10,
-						searchTime: "",
-						keyword: ''
-					},
-					data: {
-						passTime: ''
-					}
-				}
-			},
-			{
-				scrollTop: -1,
-				isComplete: false,
-				isLoading: false,
-				name: '视频',
-				items: [],
-				query: {
-					path: {
-						type: 3,
-						pageNum: 1,
-						pageSize: 10,
-						searchTime: "",
-						keyword: ''
-					},
-					data: {
-						passTime: ''
-					}
-				}
-			}
-		],
-	})
-	const changeWaterfall = (waterIndex) => {
-		if (pageData.currentIndex != waterIndex) {
-			//读取滚动条高度
-			pageData.waterfallItems[pageData.currentIndex].scrollTop = pageData.scrollTop
+import waterfall from '@/components/index/waterfall.vue'
+import { opusSearchNew } from "@/api/worksSearch/index.js"
+import { ref, onMounted, reactive, watch, computed, getCurrentInstance } from 'vue'
+import { onReachBottom, onLoad, onPageScroll, onShow } from '@dcloudio/uni-app';
+import useLoginTokenStore from '@/store/modules/loginToken'
+import _ from 'lodash'
+const searchvalue = ref('')
+const isShowHistory = ref(true)
+onMounted(() => {
+	changeWaterfall(0)
+	uni.getStorage({
+		key: 'gugusearchList',
+		success: function (res) {
+			pageData.searchHistoryList = JSON.parse(res.data)
 		}
-		pageData.currentIndex = waterIndex
-		if (pageData.waterfallItems[waterIndex].items.length == 0) {
-			getData()
-		} else {
-			//写入滚动条高度
-			if (pageData.waterfallItems[waterIndex].scrollTop != -1) {
-				uni.pageScrollTo({
-					scrollTop: pageData.waterfallItems[waterIndex].scrollTop,
-					duration: 300
-				});
-			}
+	})
+})
+const waterfallItems = [
+	{
+		scrollTop: -1, isComplete: false, isLoading: false, itemType: 'title', name: '综合', items: [],
+		query: {
+			path: { type: 1, pageNum: 1, pageSize: 10, searchTime: "", keyword: '' },
+			data: { passTime: '' }
+		}
+	},
+	{
+		scrollTop: -1, isComplete: false, isLoading: false, itemType: 'title', name: '文章', items: [], query: {
+			path: {
+				type: 2, pageNum: 1, pageSize: 10, searchTime: "", keyword: ''
+			},
+			data: { passTime: '' }
+		}
+	},
+	{
+		scrollTop: -1, isComplete: false, isLoading: false, name: '视频', items: [], query: {
+			path: {
+				type: 3, pageNum: 1, pageSize: 10, searchTime: "", keyword: ''
+			},
+			data: { passTime: '' }
 		}
 	}
-	watch(() => loginTokenStore.get().accessToken, (newVal, oldVal) => {
+]
+const pageData = reactive({
+	searchHistoryList: [],
+	scrollTop: 0,
+	currentIndex: 0,
+	waterfallItems: _.cloneDeep(waterfallItems),
+})
+watch(()=>useLoginTokenStore().get().accessToken,(newVal,oldVal)=>{
+  pageData.waterfallItems = _.cloneDeep(waterfallItems)
+  pageData.waterfallItems.forEach((item,index)=>{
+	item.query.path.keyword=searchvalue.value
+  })
+  changeWaterfall(pageData.currentIndex)
+})
+const changeWaterfall = (waterIndex) => {
+	if (pageData.currentIndex != waterIndex) {
+		//读取滚动条高度
+		pageData.waterfallItems[pageData.currentIndex].scrollTop = pageData.scrollTop
+	}
+	pageData.currentIndex = waterIndex
+	if (pageData.waterfallItems[waterIndex].items.length == 0) {
+		getData()
+	} else {
+		//写入滚动条高度
+		if (pageData.waterfallItems[waterIndex].scrollTop != -1) {
+			uni.pageScrollTo({
+				scrollTop: pageData.waterfallItems[waterIndex].scrollTop,
+				duration: 300
+			});
+		}
+	}
+}
+const getData = () => {
+	let currentIndex = pageData.currentIndex
+	pageData.waterfallItems[currentIndex].isLoading = true
+	let query = pageData.waterfallItems[currentIndex].query
+	opusSearchNew({ ...query.path }).then(res => {
+		if (res.data.page == res.data.totalPage) {
+			pageData.waterfallItems[currentIndex].isComplete = true
+		}
+		pageData.waterfallItems[currentIndex].query.path.searchTime = res.data.serviceTime
+		pageData.waterfallItems[currentIndex].items = pageData.waterfallItems[currentIndex].items.concat(res.data.list)
+		pageData.waterfallItems[currentIndex].isLoading = false
+	}).catch(e => {
+		pageData.waterfallItems[currentIndex].isLoading = false
+	})
+
+}
+onPageScroll((res) => {
+	pageData.scrollTop = res.scrollTop
+})
+const handlehistory = (item) => {
+	searchvalue.value = item
+	search()
+}
+const search = () => {
+	//将搜索记录存在本地
+	if (searchvalue.value != '') {
+		if (!pageData.searchHistoryList.includes(searchvalue.value)) {
+			pageData.searchHistoryList.unshift(searchvalue.value);
+			uni.setStorage({
+				key: 'gugusearchList',
+				data: JSON.stringify(pageData.searchHistoryList)
+			})
+		} else {
+			let i = pageData.searchHistoryList.indexOf(searchvalue.value);
+			pageData.searchHistoryList.splice(i, 1);
+			pageData.searchHistoryList.unshift(searchvalue.value);
+			uni.setStorage({
+				key: 'gugusearchList',
+				data: JSON.stringify(pageData.searchHistoryList)
+			})
+		}
+		isShowHistory.value = false
 		pageData.waterfallItems.forEach(item => {
+			item.query.path.keyword = searchvalue.value
 			item.items = []
 			item.scrollTop = -1
 		})
-		changeWaterfall(0)
-		uni.getStorage({
-			key: 'gugusearchList',
-			success: function(res) {
-				pageData.searchHistoryList = JSON.parse(res.data)
-			}
-		})
-	})
-	const getData = () => {
-		let currentIndex = pageData.currentIndex
-		pageData.waterfallItems[currentIndex].isLoading = true
-		let query = pageData.waterfallItems[currentIndex].query
-		console.log('query',query);
-		opusSearchNew({ ...query.path }).then(res => {
-			console.log('opusSearchNew',res);
-			if (res.data.page == res.data.totalPage) {
-				pageData.waterfallItems[currentIndex].isComplete = true
-			}
-			pageData.waterfallItems[currentIndex].query.path.searchTime = res.data.serviceTime
-			pageData.waterfallItems[currentIndex].items = pageData.waterfallItems[currentIndex].items.concat(res.data.list)
-			pageData.waterfallItems[currentIndex].isLoading = false
-		}).catch(e => {
-			pageData.waterfallItems[currentIndex].isLoading = false
-		})
+		getData()
+	}
+}
+const empty = () => {
+	uni.removeStorage({
+		key: "gugusearchList",
+		success: (res) => {
+			pageData.searchHistoryList = [];
+			uni.showToast({
+				title: '已清空'
+			});
+		}
+	});
 
+}
+onReachBottom(() => {
+	let currentIndex = pageData.currentIndex
+	if (!pageData.waterfallItems[currentIndex].isComplete && !pageData.waterfallItems[currentIndex].isLoading) {
+		pageData.waterfallItems[currentIndex].query.path.pageNum++
+		getData()
 	}
-	onPageScroll((res) => {
-		pageData.scrollTop = res.scrollTop
-	})
-	const handlehistory = (item) => {
-		searchvalue.value = item
-		search()
-	}
-	const search = () => {
-		//将搜索记录存在本地
-		if (searchvalue.value != '') {
-			if (!pageData.searchHistoryList.includes(searchvalue.value)) {
-				pageData.searchHistoryList.unshift(searchvalue.value);
-				uni.setStorage({
-					key: 'gugusearchList',
-					data: JSON.stringify(pageData.searchHistoryList)
-				})
-			} else {
-				let i = pageData.searchHistoryList.indexOf(searchvalue.value);
-				pageData.searchHistoryList.splice(i, 1);
-				pageData.searchHistoryList.unshift(searchvalue.value);
-				uni.setStorage({
-					key: 'gugusearchList',
-					data: JSON.stringify(pageData.searchHistoryList)
-				})
-			}
-			isShowHistory.value = false
-			pageData.waterfallItems.forEach(item => {
-				item.query.path.keyword = searchvalue.value
-				item.items = []
-				item.scrollTop = -1
-			})
-			getData()
-		}
-	}
-	const empty = () => {
-		uni.removeStorage({
-			key: "gugusearchList",
-			success: (res) => {
-				pageData.searchHistoryList = [];
-				uni.showToast({
-					title: '已清空'
-				});
-			}
-		});
-		
-	}
-	onReachBottom(() => {
-		let currentIndex = pageData.currentIndex
-		if (!pageData.waterfallItems[currentIndex].isComplete && !pageData.waterfallItems[currentIndex].isLoading) {
-			pageData.waterfallItems[currentIndex].query.path.pageNum++
-			getData()
-		}
-	})
+})
 </script>
 
 <style scoped lang="scss">
-	.uni-common-mt {
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-	}
+.uni-common-mt {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
 
-	.search {
-		width: 100%;
-		height: 30px;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		box-sizing: border-box;
-		padding: 0px 15px;
-	}
+.search {
+	width: 100%;
+	height: 30px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	box-sizing: border-box;
+	padding: 0px 15px;
+}
 
-	.searchHistory {
-		width: 100%;
-		margin-top: 5px;
-	}
+.searchHistory {
+	width: 100%;
+	margin-top: 5px;
+}
 
-	.searchHistoryItem {
-		width: 100%;
-		display: flex;
-		flex-wrap: wrap;
-	}
+.searchHistoryItem {
+	width: 100%;
+	display: flex;
+	flex-wrap: wrap;
+}
 
-	.searchHistoryItem view {
-		/* width: 50px; */
-		height: 20px;
-		border: 1px solid #eee;
-		margin: 0px 5px;
-	}
+.searchHistoryItem view {
+	/* width: 50px; */
+	height: 20px;
+	border: 1px solid #eee;
+	margin: 0px 5px;
+}
 </style>
--- src/pages/userhomepage/userhomepage.vue
+++ src/pages/userhomepage/userhomepage.vue
@@ -1,11 +1,12 @@
 <template>
-	<view >
+	<view v-if="pageInfo.mineMessage.guguId">
 		<view class="">
 			<image :src="pageInfo.mineMessage.background" class="w-full" mode="scaleToFill"></image>
 		</view>
 		<view class="bg-[#fff] w-screen  px-14 relative -top-20" style="border-radius: 35rpx 35rpx 0px 0px;">
 			<view>
-				<view class="relative bottom-24 border-2 border-[#fff] w-80 rounded-full h-80 iconShadow" @click="skipPerson">
+				<view class="relative bottom-24 border-2 border-[#fff] w-80 rounded-full h-80 iconShadow"
+					@click="skipPerson">
 					<image :src="pageInfo.mineMessage.icon" class="w-76 h-76 rounded-full "></image>
 				</view>
 				<view class="-mt-10 mb-10 font-bold text-21 flex items-center">
@@ -16,21 +17,22 @@
 				<view class="mb-10 text-14 flex items-center">
 					<image src="/static/mine/ID.png" class="w-15 h-15 "></image>
 					<text class="ml-5 mr-15">{{ pageInfo.mineMessage.guguId }}</text>
-					<image src="/static/mine/copy.png" class="w-15 h-15 ml-10" @click.stop="copy(pageInfo.mineMessage.guguId)"></image>
+					<image src="/static/mine/copy.png" class="w-15 h-15 ml-10"
+						@click.stop="copy(pageInfo.mineMessage.guguId)"></image>
 				</view>
 			</view>
 			<view class="flex text-14">
 				<view class="line relative w-60  flex items-center">
-					<view class="font-bold">{{pageInfo.mineMessage.fans}}</view>
+					<view class="font-bold">{{ pageInfo.mineMessage.fans }}</view>
 					<view class="textStyle">&nbsp;关注</view>
 				</view>
 				<view class="line relative w-60 ml-10 flex items-center">
-					<view class="font-bold">{{pageInfo.mineMessage.focus}}</view>
+					<view class="font-bold">{{ pageInfo.mineMessage.focus }}</view>
 
 					<view class="textStyle">&nbsp;收藏</view>
 				</view>
 				<view class="w-60  ml-10 flex items-center">
-					<view class="font-bold">{{pageInfo.mineMessage.liked}}</view>
+					<view class="font-bold">{{ pageInfo.mineMessage.liked }}</view>
 					<view class="textStyle">&nbsp;获赞</view>
 				</view>
 			</view>
@@ -39,7 +41,7 @@
 					<view class="info">
 						<view :class="{ hide: !iSinfo }">
 							<view v-if="pageInfo.mineMessage.introduce">
-								{{pageInfo.mineMessage.introduce}}
+								{{ pageInfo.mineMessage.introduce }}
 							</view>
 							<view v-else>
 								这家伙很懒，连介绍都没写~~
@@ -55,8 +57,10 @@
 		<!-- 菜单 -->
 		<view class="sticky -top-5 z-50 bg-[#fff] ml-14 w-full  -mt-25 pt-16">
 			<view class="flex ">
-				<view v-for="(waterItem, index) in pageData.waterfallItems"  :key="index"   class="mr-26" @click="changeWaterfall(index)">
-					<view :class="pageData.currentIndex == index?'active':'inactive'">{{waterItem.name}} <text v-if="waterItem.query.data.totalCount">({{waterItem.query.data.totalCount}})</text></view>
+				<view v-for="(waterItem, index) in pageData.waterfallItems" :key="index" class="mr-26"
+					@click="changeWaterfall(index)">
+					<view :class="pageData.currentIndex == index ? 'active' : 'inactive'">{{ waterItem.name }} <text
+							v-if="waterItem.query.data.totalCount">({{ waterItem.query.data.totalCount }})</text></view>
 					<view class="-mt-5">
 						<image src="/static/mine/line.png" class="w-34 h-4 " v-show="pageData.currentIndex == index" />
 					</view>
@@ -65,17 +69,15 @@
 		</view>
 
 		<!-- 菜单 -->
-		<view  class="pt-13 bg-gray-100">
-			<view v-for="(waterItem,waterIndex) in pageData.waterfallItems" :key="waterIndex">
-				<view  v-if="!waterItem.items.length&&waterIndex == pageData.currentIndex">
-				    <u-empty
-				            mode="list"
-				            icon="http://cdn.uviewui.com/uview/empty/list.png"
-				    >
-				    </u-empty>
+		<view class="pt-13 bg-gray-100">
+			<view v-for="(waterItem, waterIndex) in pageData.waterfallItems" :key="waterIndex">
+				<view v-if="!waterItem.items.length && waterIndex == pageData.currentIndex">
+					<u-empty mode="list" icon="http://cdn.uviewui.com/uview/empty/list.png">
+					</u-empty>
 				</view>
 				<view v-show="waterIndex == pageData.currentIndex">
-					<waterfall :isComplete="waterItem.isComplete" :itemType="waterItem.itemType" :value="waterItem.items" :waterIndex="waterIndex" :currentIndex="pageData.currentIndex" itemKey="mine">
+					<waterfall :isComplete="waterItem.isComplete" :itemType="waterItem.itemType" :value="waterItem.items"
+						:waterIndex="waterIndex" :currentIndex="pageData.currentIndex" itemKey="mine">
 					</waterfall>
 				</view>
 			</view>
@@ -84,238 +86,203 @@
 </template>
 
 <script setup>
-	import waterfall from '@/components/index/waterfall.vue'
-	import { userhomepage, homepageopus } from "@/api/mine/index.js"
-	import { ref, onMounted, reactive, watch, computed, getCurrentInstance } from 'vue'
-	import { onShow, onReachBottom, onPageScroll, onLoad } from "@dcloudio/uni-app"
-	import { needLogin } from "@/utils/utils"
-	import useLoginTokenStore from '@/store/modules/loginToken'
-	const loginTokenStore = useLoginTokenStore()
-	const waterlist = ref([])
-	const pageData = reactive({
-		masterId: '',
-		scrollTop: 0,
-		currentIndex: 0,
-		waterfallItems: [{
-			scrollTop: -1,
-			isComplete: false,
-			isLoading: false,
-			itemType: 'title',
-			name: '作品',
-			items: [],
-			query: {
-				path: {
-					pageNum: 1,
-					pageSize: 10,
-				},
-				data: {
-					totalCount: ''
-				}
-			}
-		}],
-	})
-	onLoad((option) => {
-		pageData.masterId = option.id
-		userhomepage({
-			masterId: pageData.masterId
-		}).then(reslove => {
-			pageInfo.mineMessage = reslove.data.userInfo
-			changeWaterfall(0)
-		})
-	})
-	watch(() => loginTokenStore.get().accessToken, (newVal, oldVal) => {
-		pageData.waterfallItems=[{
-			scrollTop: -1,
-			isComplete: false,
-			isLoading: false,
-			itemType: 'title',
-			name: '作品',
-			items: [],
-			query: {
-				path: {
-					pageNum: 1,
-					pageSize: 10,
-				},
-				data: {
-					totalCount: ''
-				}
-			}
-		}]
-		userhomepage({
-			masterId: pageData.masterId
-		}).then(reslove => {
-			pageInfo.mineMessage = reslove.data.userInfo
-			changeWaterfall(0)
-		})
-	})
-	const internalInstance = getCurrentInstance()
-	const iSinfo = ref(false)
-	const pageInfo = reactive({
-		//个人信息数据
-		mineMessage: {}
-	})
-	const changeWaterfall = (waterIndex) => {
-		if (pageData.currentIndex != waterIndex) {
-			//读取滚动条高度
-			pageData.waterfallItems[pageData.currentIndex].scrollTop = pageData.scrollTop
-		}
-		pageData.currentIndex = waterIndex
-		if (pageData.waterfallItems[waterIndex].items.length == 0) {
-			getData()
-		} else {
-			//写入滚动条高度
-			if (pageData.waterfallItems[waterIndex].scrollTop != -1) {
-			  uni.pageScrollTo({
-			    scrollTop: pageData.waterfallItems[waterIndex].scrollTop,
-			    duration: 300
-			  });
-			}
-		}
+import waterfall from '@/components/index/waterfall.vue'
+import { userhomepage, homepageopus } from "@/api/mine/index.js"
+import { ref, onMounted, reactive, watch, computed, getCurrentInstance } from 'vue'
+import { onShow, onReachBottom, onPageScroll, onLoad } from "@dcloudio/uni-app"
+import useLoginTokenStore from '@/store/modules/loginToken'
+import _ from 'lodash'
+const waterfallItems = [{
+	scrollTop: -1, isComplete: false, isLoading: false, itemType: 'title', name: '作品', items: [],
+	query: {
+		path: { pageNum: 1, pageSize: 10, },
+		data: { totalCount: '' }
 	}
-	const getData = () => {
-		let currentIndex = pageData.currentIndex
-		pageData.waterfallItems[currentIndex].isLoading = true
-		let query = pageData.waterfallItems[currentIndex].query
-		homepageopus({
-			masterId: pageData.masterId,
-			...query.path
-		}).then(res => {
-			if (res.data.page == res.data.totalPage) {
-				pageData.waterfallItems[currentIndex].isComplete = true
-			}
-			pageData.waterfallItems[currentIndex].query.data.totalCount = res.data.totalCount
-			pageData.waterfallItems[currentIndex].items = pageData.waterfallItems[currentIndex].items.concat(res.data.list)
-			pageData.waterfallItems[currentIndex].isLoading = false
-		}).catch(e => {
-			pageData.waterfallItems[currentIndex].isLoading = false
-		})
-	}
-	onPageScroll((res) => {
-		pageData.scrollTop = res.scrollTop
+}]
+const pageData = reactive({
+	masterId: '',
+	scrollTop: 0,
+	currentIndex: 0,
+	waterfallItems: _.cloneDeep(waterfallItems),
+})
+watch(() => useLoginTokenStore().get().accessToken, (newVal, oldVal) => {
+	pageData.waterfallItems = _.cloneDeep(waterfallItems)
+	changeWaterfall(pageData.currentIndex)
+})
+onLoad((option) => {
+	pageData.masterId = option.id
+	userhomepage({
+		masterId: pageData.masterId
+	}).then(reslove => {
+		pageInfo.mineMessage = reslove.data.userInfo
+		changeWaterfall(0)
 	})
-	onReachBottom(() => {
-		let currentIndex = pageData.currentIndex
-		if (!pageData.waterfallItems[currentIndex].isComplete && !pageData.waterfallItems[currentIndex].isLoading) {
-			pageData.waterfallItems[currentIndex].query.path.pageNum++
-			getData()
-		}
-	})
+})
 
-	function copy(value) {
-		uni.setClipboardData({
-			data: value, //要被复制的内容
-			success: () => { //复制成功的回调函数
-				uni.showToast({ //提示
-					title: '复制成功'
-				})
-			}
-		});
+const iSinfo = ref(false)
+const pageInfo = reactive({
+	//个人信息数据
+	mineMessage: {}
+})
+const changeWaterfall = (waterIndex) => {
+	if (pageData.currentIndex != waterIndex) {
+		//读取滚动条高度
+		pageData.waterfallItems[pageData.currentIndex].scrollTop = pageData.scrollTop
 	}
-	// 数据赋值
-	const showinfo = () => {
-		iSinfo.value = !iSinfo.value
+	pageData.currentIndex = waterIndex
+	if (pageData.waterfallItems[waterIndex].items.length == 0) {
+		getData()
+	} else {
+		//写入滚动条高度
+		if (pageData.waterfallItems[waterIndex].scrollTop != -1) {
+			uni.pageScrollTo({
+				scrollTop: pageData.waterfallItems[waterIndex].scrollTop,
+				duration: 300
+			});
+		}
 	}
-	const skipPerson = () => {
-		uni.navigateTo({
-			url: '/pages/personCenter/personCenter'
-		})
+}
+const getData = () => {
+	let currentIndex = pageData.currentIndex
+	pageData.waterfallItems[currentIndex].isLoading = true
+	let query = pageData.waterfallItems[currentIndex].query
+	homepageopus({ masterId: pageData.masterId, ...query.path }).then(res => {
+		if (res.data.page == res.data.totalPage) {
+			pageData.waterfallItems[currentIndex].isComplete = true
+		}
+		pageData.waterfallItems[currentIndex].query.data.totalCount = res.data.totalCount
+		pageData.waterfallItems[currentIndex].items = pageData.waterfallItems[currentIndex].items.concat(res.data.list)
+		pageData.waterfallItems[currentIndex].isLoading = false
+	}).catch(e => {
+		pageData.waterfallItems[currentIndex].isLoading = false
+	})
+}
+onPageScroll((res) => {
+	pageData.scrollTop = res.scrollTop
+})
+onReachBottom(() => {
+	let currentIndex = pageData.currentIndex
+	if (!pageData.waterfallItems[currentIndex].isComplete && !pageData.waterfallItems[currentIndex].isLoading) {
+		pageData.waterfallItems[currentIndex].query.path.pageNum++
+		getData()
 	}
+})
+
+function copy(value) {
+	uni.setClipboardData({
+		data: value, //要被复制的内容
+		success: () => { //复制成功的回调函数
+			uni.showToast({ //提示
+				title: '复制成功'
+			})
+		}
+	});
+}
+// 数据赋值
+const showinfo = () => {
+	iSinfo.value = !iSinfo.value
+}
+const skipPerson = () => {
+	uni.navigateTo({
+		url: '/pages/personCenter/personCenter'
+	})
+}
 </script>
 
 <style lang="scss" scoped>
-	.active {
-		font-size: 17px;
-		font-family: Source Han Sans SC;
-		font-weight: bold;
-		color: #272A29;
-	}
+.active {
+	font-size: 17px;
+	font-family: Source Han Sans SC;
+	font-weight: bold;
+	color: #272A29;
+}
 
-	.inactive {
-		font-size: 16px;
-		font-family: Source Han Sans SC;
-		font-weight: 400;
-		color: #666666;
-	}
+.inactive {
+	font-size: 16px;
+	font-family: Source Han Sans SC;
+	font-weight: 400;
+	color: #666666;
+}
 
-	.iconShadow {
-		box-shadow: 0rpx 1rpx 18rpx 0rpx rgba(0, 0, 0, 0.19);
-	}
+.iconShadow {
+	box-shadow: 0rpx 1rpx 18rpx 0rpx rgba(0, 0, 0, 0.19);
+}
 
-	.textStyle {
-		font-family: Source Han Sans SC;
-		font-weight: 300;
-		color: #999999;
-	}
+.textStyle {
+	font-family: Source Han Sans SC;
+	font-weight: 300;
+	color: #999999;
+}
 
-	.Express {
-		line-height: 24rpx;
-		font-family: Source Han Sans SC;
-		font-weight: 300;
-		color: #999999;
+.Express {
+	line-height: 24rpx;
+	font-family: Source Han Sans SC;
+	font-weight: 300;
+	color: #999999;
+	display: flex;
+	flex-direction: column;
+	padding: 30upx 0;
+	position: relative;
+
+	.info {
 		display: flex;
 		flex-direction: column;
-		padding: 30upx 0;
-		position: relative;
 
-		.info {
-			display: flex;
-			flex-direction: column;
-
-			view {
-				text-align: justify;
-				font-family: PingFangSC-Regular, PingFang SC;
-				font-weight: 400;
-				word-break: break-word; //换行模式
-			}
-
-			text {
-				width: 70px;
-				display: flex;
-				justify-content: flex-end;
-				align-items: center;
-				color: #0078FF;
-				position: absolute;
-				bottom: 30upx;
-				right: 30upx;
-			}
-		}
-
-	}
-
-	.hidebtn {
-		display: flex;
-		flex: 1;
-		justify-content: flex-end;
-		color: #0078FF;
-
-	}
-
-	.hide {
-		word-break: break-word; //换行模式
-		overflow: hidden;
-		text-overflow: ellipsis; //修剪文字
-		display: -webkit-box;
-		-webkit-line-clamp: 2; //此处为上限行数
-		-webkit-box-orient: vertical;
-	}
-
-	.collectView {
 		view {
-			border-radius: 20%;
+			text-align: justify;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			word-break: break-word; //换行模式
+		}
+
+		text {
+			width: 70px;
+			display: flex;
+			justify-content: flex-end;
+			align-items: center;
+			color: #0078FF;
+			position: absolute;
+			bottom: 30upx;
+			right: 30upx;
 		}
 	}
 
-	.line::after {
-		position: absolute;
-		left: 40%;
-		bottom: 20%;
-		z-index: 11;
-		width: 100%;
-		height: 100%;
-		content: "";
-		display: block;
-		border-right: 1px solid gray;
-		transform: scale(0.5);
-		transform-origin: left bottom;
+}
+
+.hidebtn {
+	display: flex;
+	flex: 1;
+	justify-content: flex-end;
+	color: #0078FF;
+
+}
+
+.hide {
+	word-break: break-word; //换行模式
+	overflow: hidden;
+	text-overflow: ellipsis; //修剪文字
+	display: -webkit-box;
+	-webkit-line-clamp: 2; //此处为上限行数
+	-webkit-box-orient: vertical;
+}
+
+.collectView {
+	view {
+		border-radius: 20%;
 	}
+}
+
+.line::after {
+	position: absolute;
+	left: 40%;
+	bottom: 20%;
+	z-index: 11;
+	width: 100%;
+	height: 100%;
+	content: "";
+	display: block;
+	border-right: 1px solid gray;
+	transform: scale(0.5);
+	transform-origin: left bottom;
+}
 </style>
