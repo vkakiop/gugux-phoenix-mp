1b80d2eeccd64f957aa37818d7ba9bcefd030d3f
lww <1532329365@qq.com>
2023-06-20 15:46:37 +0800

    去除代码间距

--- src/pages/personCenter/personCenter.vue
+++ src/pages/personCenter/personCenter.vue
@@ -7,7 +7,7 @@
 					<view class="mr-8">
 						<image :src="pageData.userInfo.icon" class="w-40 h-40 rounded-full"> </image>
 					</view>
-				<!-- 	<view class=""> <uni-icons type="right" size="20"></uni-icons></view> -->
+					<!-- 	<view class=""> <uni-icons type="right" size="20"></uni-icons></view> -->
 				</view>
 			</view>
 			<view class="flex justify-between border-0 p-10  ">
@@ -23,14 +23,15 @@
 			<view class="flex justify-between   p-10 ">
 				<view class="">手机号</view>
 				<view class="text-14 text-[#999] font-light flex items-center">
-					<view class="mr-8"><text> {{pageData.userInfo.phone }}</text></view>
+					<view class="mr-8"><text> {{ pageData.userInfo.phone }}</text></view>
 					<!-- <view class=""> <uni-icons type="right" size="20"></uni-icons></view> -->
 				</view>
 			</view>
 			<view class="flex justify-between p-10 ">
 				<view class="">咕咕号</view>
 				<view class="text-14 text-[#999] font-light">
-					{{ pageData.userInfo.guguId }}<text class="text-[#F8CF01] ml-11" @click="copy(pageData.userInfo.guguId)"> 复制</text>
+					{{ pageData.userInfo.guguId }}<text class="text-[#F8CF01] ml-11"
+						@click="copy(pageData.userInfo.guguId)"> 复制</text>
 				</view>
 			</view>
 			<!-- <view class="flex justify-between p-10 ">
@@ -43,7 +44,7 @@
 			<view class="flex justify-between  p-10 ">
 				<view class="">签名</view>
 				<view class="text-14 text-[#999] font-light flex items-center">
-					<view class="mr-8">{{pageData.userInfo.introduce?pageData.userInfo.introduce:'未设置'}}</view>
+					<view class="mr-8">{{ pageData.userInfo.introduce ? pageData.userInfo.introduce : '未设置' }}</view>
 					<!-- <view class=""> <uni-icons type="right" size="20"></uni-icons></view> -->
 				</view>
 			</view>
@@ -51,8 +52,8 @@
 				<view class="">性别</view>
 				<view class="text-14 text-[#999] font-light flex items-center">
 					<view class="mr-8">
-						<text v-if="pageData.userInfo.sex===0">保密</text>
-						<text v-else-if="pageData.userInfo.sex===1">男</text>
+						<text v-if="pageData.userInfo.sex === 0">保密</text>
+						<text v-else-if="pageData.userInfo.sex === 1">男</text>
 						<text v-else>女</text>
 					</view>
 					<!-- <view class=""> <uni-icons type="right" size="20"></uni-icons></view> -->
@@ -61,18 +62,18 @@
 			<view class="flex justify-between  p-10 ">
 				<view class="">出生日期</view>
 				<view class="text-14 text-[#999] font-light flex items-center">
-					<view class="mr-8">{{pageData.userInfo.birth?pageData.userInfo.birth:'未设置'}}</view>
+					<view class="mr-8">{{ pageData.userInfo.birth ? pageData.userInfo.birth : '未设置' }}</view>
 					<!-- <view class=""> <uni-icons type="right" size="20"></uni-icons></view> -->
 				</view>
 			</view>
 			<view class="flex justify-between  p-10 ">
 				<view class="">所在城市</view>
 				<view class="text-14 text-[#999] font-light flex items-center ">
-					<view class="mr-8">{{pageData.userInfo.city?pageData.userInfo.city:'未设置'}}</view>
+					<view class="mr-8">{{ pageData.userInfo.city ? pageData.userInfo.city : '未设置' }}</view>
 					<!-- <view class=""> <uni-icons type="right" size="20"></uni-icons></view> -->
 				</view>
 			</view>
-		<!-- 	<view class="flex justify-between text-700 px-10 pb-10  items-center">
+			<!-- 	<view class="flex justify-between text-700 px-10 pb-10  items-center">
 				<view class="">名片</view>
 				<view class="flex items-center">
 					<view class="mr-8">
@@ -89,64 +90,57 @@
 </template>
 
 <script setup>
-	import {
-		getUserInfo
-	} from "@/api/mine/index.js"
-	import {
-		onLoad
-	} from "@dcloudio/uni-app";
-	import {
-		ref,
-		reactive
-	} from 'vue'
-	const pageData = reactive({
-		//数据全部列表
-		userInfo: {}
+import {getUserInfo} from "@/api/mine/index.js"
+import {onLoad} from "@dcloudio/uni-app";
+import {ref,reactive} from 'vue'
+const pageData = reactive({
+	//数据全部列表
+	userInfo: {}
+})
+onLoad(() => {
+	getUserInfo({}).then(res => {
+		pageData.userInfo = {
+			...res.data
+		}
 	})
-	onLoad(() => {
-		getUserInfo({}).then(res => {
-			pageData.userInfo = {
-				...res.data
-			}
-		})
+})
+const popup = ref()
+const skipQRcode = () => {
+	uni.navigateTo({
+		url: `/pages/personCenter/personQRcode?id=${pageData.userInfo.id}`
 	})
-	const popup = ref()
-	const skipQRcode = () => {
-		uni.navigateTo({
-			url: `/pages/personCenter/personQRcode?id=${pageData.userInfo.id}`
-		})
-	}
+}
 
-	function copy(value) {
-		uni.setClipboardData({
-			data: value, //要被复制的内容
-			success: () => { //复制成功的回调函数
-				uni.showToast({ //提示
-					title: '复制成功'
+function copy(value) {
+	uni.setClipboardData({
+		data: value, //要被复制的内容
+		success: () => { //复制成功的回调函数
+			uni.showToast({ //提示
+				title: '复制成功'
+			})
+		}
+	});
+}
+const logOff = () => {
+	uni.showModal({
+		title: '提示',
+		content: '确定退出当前状态',
+		cancelText: '确定退出',
+		confirmText: '在逛一会',
+		success: function (res) {
+			if (res.cancel) {
+				uni.redirectTo({
+					url: '/pages/login/logout'
 				})
 			}
-		});
-	}
-	const logOff = () => {
-		uni.showModal({
-			title: '提示',
-			content: '确定退出当前状态',
-			cancelText: '确定退出',
-			confirmText: '在逛一会',
-			success: function(res) {
-				if (res.cancel) {
-					uni.redirectTo({
-						url: '/pages/login/logout'
-					})
-				}
-			}
-		});
-	}
+		}
+	});
+}
 </script>
 
 <style lang="scss" scoped>
-	.content>view {
-		background-color: #fff;
-		color: #272A29;
-	}
+.content>view {
+	background-color: #fff;
+	color: #272A29;
+}
 </style>
