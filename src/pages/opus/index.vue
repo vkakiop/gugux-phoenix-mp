a872c9639e691fb06426c1c485c0f4c5b175fdc4
qintao <qintao@test.com>
2023-06-25 11:52:52 +0800

    详情页面添加traceInfo

--- src/pages/opus/index.vue
+++ src/pages/opus/index.vue
@@ -12,7 +12,7 @@
       </customNav>
       <opus-article :detail="pageData.detail" v-if="pageData.detail.opusType == 1"></opus-article>
       <!--opus-video :detail="pageData.detail" v-else-if="pageData.detail.opusType == 2"></opus-video-->
-      <comment ref="commentRef" :id="pageData.detail.id"></comment>
+      <view id="comment_list"><comment ref="commentRef" :id="pageData.detail.id" :articleType="pageData.isShowCommentPage ? 2 : 1"></comment></view>
       <view class="h-50"></view>
       <view class="fixed bottom-0 h-50 w-screen bg-white">
         <view class="mx-20 flex justify-between mt-12">
@@ -27,39 +27,54 @@
                 <view class="ml-5 mt-2">{{pageData.detail.likeNum}}</view>
               </view>
             </debounce>
-            <debounce @debounce="commentAdd">
-              <view class="flex mr-20">
-                <image class="w-24 h-24" src="@/static/opus/icon_comment.png"/>
-                <view class="ml-5 mt-2">{{pageData.detail.commentNum}}</view>
-              </view>
-            </debounce>
             <debounce @debounce="collection">
-              <view class="flex mr-0">
+              <view class="flex mr-20">
                 <image v-if="pageData.detail.isCollection" class="w-24 h-24" src="@/static/opus/icon_collect_ed.png"/>
                 <image v-else class="w-24 h-24" src="@/static/opus/icon_collect.png"/>
                 <view class="ml-5 mt-2">{{pageData.detail.collectionNum}}</view>
               </view>
             </debounce>
+            <debounce @debounce="commentAdd">
+              <view class="flex mr-0">
+                <image class="w-24 h-24" src="@/static/opus/icon_comment.png"/>
+                <view class="ml-5 mt-2">{{pageData.detail.commentNum}}</view>
+              </view>
+            </debounce>
           </view>
         </view>
       </view>
+      <u-popup :show="pageData.isShowCommentPop" @close="pageData.isShowCommentPop = false">
+        <view class="container">
+          <comment ref="comment2Ref" :id="pageData.detail.id" :articleType="2"></comment>
+        </view>
+        <view
+            class="flex items-center ml-14  mb-14 bg-[#f5f6f8]  h-39 w-350 rounded-19 border-1 border-[#E3E3E3] text-14"
+            @click="comment2Add">
+          <icon type="search" size="11" class="mx-10" />
+          <input class="bg-[#f5f6f8]" placeholder="说点什么..." type="text" disabled />
+        </view>
+      </u-popup>
       <loginPop :isShow="pageData.isShowLoginPop" @close="pageData.isShowLoginPop = false"></loginPop>
     </view>
 </template>
 
 <script setup>
-import { reactive,ref,watch } from "vue"
+import {reactive, ref, watch, getCurrentInstance, nextTick} from "vue"
 import { opusInfo,opusCollect,opusLike,userFans,userFansRemove } from "@/api/opus/index"
 import { getTokenValue } from "@/utils/utils"
 import {onLoad} from '@dcloudio/uni-app'
 import opusArticle from './components/opusArticle'
 import comment from "@/components/common/comment.vue"
 import useLoginTokenStore from '@/store/modules/loginToken'
+
 const loginTokenStore = useLoginTokenStore()
 const commentRef = ref()
+const comment2Ref = ref()
+const _this = getCurrentInstance()
 
 onLoad((option)=>{
     pageData.id = option.id
+    pageData.traceInfo = decodeURIComponent(option.traceInfo || '')
     getData()
 })
 
@@ -70,7 +85,11 @@ watch(()=>loginTokenStore.get().accessToken,(newVal,oldVal)=>{
 const pageData = reactive({
   //id: '1601683533353328803',
   id:'',
+  traceInfo:'',
   isShowLoginPop:false,
+  isShowCommentPage:true,
+  isShowCommentPop:false,
+  observeDistance:0,
   detail: {
     cover: {},
     opusType: 1,
@@ -157,7 +176,16 @@ const like = ()=>{
 }
 
 const commentAdd = ()=>{
-  commentRef.value.init(true)
+  if (pageData.isShowCommentPage) {
+    commentRef.value.init(true)
+  }
+  else {
+    pageData.isShowCommentPop = true
+  }
+}
+
+const comment2Add = ()=>{
+  comment2Ref.value.init(true)
 }
 
 const gotoBack = ()=>{
@@ -175,12 +203,15 @@ const gotoMine = ()=>{
 }
 
 const getData = ()=>{
-  getDataApi({opusId:pageData.id})
+  getDataApi({opusId:pageData.id,traceInfo:pageData.traceInfo})
 }
 
 const getDataApi = (params)=>{
     opusInfo(params).then((res)=>{
         pageData.detail = res.data
+      nextTick(()=>{
+        observePage()
+      })
     })
 }
 
@@ -194,5 +225,15 @@ const onShareAppMessage = ()=>{
 
 const onShareTimeline = ()=>{
   return onShareAppMessage()
+}
+
+const observePage = () => {
+  let IntersectionObserver = uni.createIntersectionObserver(_this)
+  IntersectionObserver.disconnect()
+  IntersectionObserver = uni.createIntersectionObserver(_this)
+  IntersectionObserver.relativeToViewport({ top: pageData.observeDistance, bottom: pageData.observeDistance })
+      .observe(`#comment_list`, ({ intersectionRatio }) => {
+        //pageData.isShowCommentPage = intersectionRatio > 0
+      })
 }
 </script>
