f8934ddd0e27b60b096cdff6092d4d49f86656e1
qintao <qintao@test.com>
2023-06-26 09:19:55 +0800

    表情图标优化

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,7 @@
+//内部版本号
+export const innserVersion = 1
+
+
 //配置判断是否SwitchTab
 export const switchTabPathes = [
     '/pages/index/index',
@@ -6,3 +10,7 @@ export const switchTabPathes = [
 
 //存储token名称
 export const configLoginToken = 'ggx_login_token'
+
+
+//表情图标位置
+export const configCommentFaceUrl = 'https://gugux.caigetuxun.com/v3/xcx/static/comment/facesnew/'
