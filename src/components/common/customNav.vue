ce7628cc739fb19f85debd2b433d12378ff96185
qintao <qintao@test.com>
2023-06-29 14:17:53 +0800

    自定义导航条

--- src/components/common/customNav.vue
+++ src/components/common/customNav.vue
@@ -6,26 +6,20 @@
         <slot></slot>
       </view>
     </view>
-    <view :style="{height:((pageData.statusBarHeight + pageData.titleBarHeight)/screen )+'px'}"></view>
+    <view :style="{height:(pageData.height )+'px'}"></view>
   </view>
 </template>
 
 <script setup>
 import {onMounted, reactive} from "vue"
 
-const props = defineProps({
-  screen: {
-    type:Number,
-    default:1
-  }
-})
-
-const emit = defineEmits(['heightInfo'])
+const emit = defineEmits(['changeHeightInfo'])
 
 const pageData = reactive({
-  statusBarHeight: 0,
-  titleBarHeight: 0,
+  statusBarHeight: 0,//状态栏高度
+  titleBarHeight: 0,//导航栏高度
   titleBarRight: 0,
+  height:0, //状态栏+导航栏 总高度
   isShow: false,
 })
 
@@ -35,10 +29,10 @@ onMounted(()=> {
   let menuButtonInfo = uni.getMenuButtonBoundingClientRect()
   pageData.titleBarHeight = (menuButtonInfo.top - pageData.statusBarHeight) * 2 + menuButtonInfo.height
   pageData.titleBarRight = menuButtonInfo.width + (systemInfo.windowWidth - menuButtonInfo.right)
-
+  pageData.height = pageData.statusBarHeight + pageData.titleBarHeight
   pageData.isShow = true
 
-  emit('heightInfo',pageData)
+  emit('changeHeightInfo',pageData)
 })
 </script>
 
