5bd0beff38e33456df66edba963c828afe39a24a
qintao <qintao@test.com>
2023-07-21 11:33:21 +0800

    所有图片优化到远程访问

--- src/components/chat/msglist/msglist.vue
+++ src/components/chat/msglist/msglist.vue
@@ -80,7 +80,7 @@
               <image
                 v-if="d_item.type == 'emoji'"
                 class="avatar"
-                :src="'/static/images/faces/' + d_item.data"
+                :src="configStaticPath('/static/images/faces/' + d_item.data)"
                 style="width:25px; height:25px; margin:0 0 2px 0; float:left;"
               />
             </view>
