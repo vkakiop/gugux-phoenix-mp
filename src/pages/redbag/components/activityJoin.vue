de7cab223b85947a4e9d8cf7fe75c7aec7b99a9d
qintao <qintao@test.com>
2023-08-30 13:31:43 +0800

    小程序红包

---
+++ src/pages/redbag/components/activityJoin.vue
@@ -0,0 +1,28 @@
+<template>
+  <view>
+    <view v-if="pageData.isShow">
+      活动按钮
+    </view>
+  </view>
+</template>
+
+<script setup>
+import {reactive} from "vue";
+
+const pageData = reactive({
+  isShow:false,
+})
+
+const init = (row) => {
+  if (row) {
+    uni.showToast({title: 'test',icon:'none',duration: 2000
+    });
+    pageData.isShow = true
+  }
+}
+defineExpose({ init })
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/pages/redbag/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view>
+    <view>红包首页</view>
+    <button @click="onRedbagOpen">打开</button>
+    <activityJoin ref="activityJoinRef"></activityJoin>
+  </view>
+</template>
+
+<script setup>
+import {ref,reactive} from 'vue'
+import activityJoin from './components/activityJoin.vue'
+
+const activityJoinRef = ref()
+const onRedbagOpen = ()=>{
+  activityJoinRef.value.init({})
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
